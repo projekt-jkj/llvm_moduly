3060c3378949e1804ac6d4654cee67daaab14dab
Basvas j.k.j <basvas@seznam.cz>
2026-04-28 22:06:40 +0200

    Úprava struktury projektu. Aktualizace na LLVM 22.1.4

--- init_repositories.sh
+++ init_repositories.sh
@@ -2,7 +2,7 @@
 
 set -ue;
 
-LLVM_TAG="llvmorg-22.1.3";
+LLVM_TAG="llvmorg-22.1.4";
 MINGW_DEFAULT_TAG="v14.0.0";
 
 for a in "$@"
@@ -33,12 +33,12 @@ clone()
 }
 
 LLVM_DIR="llvm-project_$LLVM_TAG";
-clone "$LLVM_TAG" "https://github.com/llvm/llvm-project.git" "$LLVM_DIR";
+clone "$LLVM_TAG" "https://github.com/llvm/llvm-project.git" "src/$LLVM_DIR";
 echo "LLVM_DIR=$LLVM_DIR";
 
 if [ -v MINGW_TAG ]
 then
     MINGW_DIR="mingw-w64_$MINGW_TAG";
-    clone "$MINGW_TAG" "https://github.com/mingw-w64/mingw-w64.git" "$MINGW_DIR";
+    clone "$MINGW_TAG" "https://github.com/mingw-w64/mingw-w64.git" "src/$MINGW_DIR";
     echo "MINGW_DIR=$MINGW_DIR";
 fi
