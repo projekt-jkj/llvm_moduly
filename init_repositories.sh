27f92ee76b20003d1ef4cd6a2dc5288ed1eda3ed
Basvas j.k.j <basvas@seznam.cz>
2026-05-02 22:36:44 +0200

    Úprava init_repositories.sh: zahrnutí def.sh

--- init_repositories.sh
+++ init_repositories.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
-
 set -ue;
-
-LLVM_TAG="llvmorg-22.1.4";
-MINGW_DEFAULT_TAG="v14.0.0";
 
 for a in "$@"
 do
 case $a in
-    -llvm=*)
-        LLVM_TAG="${a#*=}";
-        ;;
-    -mingw)
-        MINGW_TAG="$MINGW_DEFAULT_TAG";
-        ;;
-    -mingw=*)
-        MINGW_TAG="${a#*=}";
-        ;;
-    *)
-        echo "Unknown argument '$a'";
-        exit 1;
-        ;;
+	-llvm=*)
+		LLVM_TAG="${a#*=}";
+		;;
+	-mingw)
+		INCLUDE_MINGW="ON";
+		;;
+	-mingw=*)
+		INCLUDE_MINGW="ON";
+		MINGW_TAG="${a#*=}";
+		;;
+	-log=*)
+		LOG_FILE="${a#*=}";
+		;;
+	*)
+		echo "Unknown argument '$a'";
+		exit 1;
+		;;
 esac
 done
 
+source ./def.sh;
+
 clone()
 {
-    if [ ! -d "$3" ]
-    then
-        git clone --depth 1 -b "$1" "$2" "$3";
-    fi
+	log_begin "Cloning $2";
+	if [ ! -d "$3" ]
+	then
+		git clone --depth 1 -b "$1" "$2" "$3" >"$LOG_FILE";
+	fi
+	log_ok "Cloning $2";
 }
 
-LLVM_DIR="llvm-project_$LLVM_TAG";
-clone "$LLVM_TAG" "https://github.com/llvm/llvm-project.git" "src/$LLVM_DIR";
-echo "LLVM_DIR=$LLVM_DIR";
+clone "$LLVM_TAG" "https://github.com/llvm/llvm-project.git" "$LLVM_SOURCE";
 
 if [ -v MINGW_TAG ]
 then
-    MINGW_DIR="mingw-w64_$MINGW_TAG";
-    clone "$MINGW_TAG" "https://github.com/mingw-w64/mingw-w64.git" "src/$MINGW_DIR";
-    echo "MINGW_DIR=$MINGW_DIR";
+	clone "$MINGW_TAG" "https://github.com/mingw-w64/mingw-w64.git" "$MINGW_SOURCE";
 fi
