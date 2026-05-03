c088307c2e763c383b1be92ff1a016114b53f4f7
Basvas j.k.j <basvas@seznam.cz>
2026-05-03 21:53:59 +0200

    Přeuspořádání proměnných v def.sh

--- def.sh
+++ def.sh
@@ -1,21 +1,21 @@
-# versions
+# LLVM variables
 
 LLVM_MODULY_VERSION="${LLVM_MODULY_VERSION:-22.1.5}";
 LLVM_TAG="${LLVM_TAG:-llvmorg-22.1.5}";
-MINGW_DEFAULT_TAG="v14.0.0";
+LLVM_SOURCE="$(pwd)/src/llvm_${LLVM_TAG}";
+LLVM_BUILD="$(pwd)/build/llvm_${LLVM_TAG}";
+
+# MinGW variables
 
 if [ -n "${INCLUDE_MINGW:-}" ]
 then
+	MINGW_DEFAULT_TAG="v14.0.0";
     MINGW_TAG="${MINGW_TAG:-$MINGW_DEFAULT_TAG}";
+	MINGW_SOURCE="$(pwd)/src/mingw_${MINGW_TAG}";
+	MINGW_BUILD="$(pwd)/build/mingw_${MINGW_TAG}";
 fi
 
-# source code, build and install folders
-
-LLVM_SOURCE="$(pwd)/src/llvm_${LLVM_TAG}";
-MINGW_SOURCE="$(pwd)/src/mingw_${MINGW_TAG}";
-
-LLVM_BUILD="$(pwd)/build/llvm_${LLVM_TAG}";
-MINGW_BUILD="$(pwd)/build/mingw_${MINGW_TAG}";
+# install variables
 
 INSTALL_BASE="$(pwd)/install_$LLVM_MODULY_VERSION";
 
@@ -25,9 +25,9 @@ LOG_FILE="/dev/null";
 
 log_begin()
 {
-	echo "=== [$1] $(date '+%H:%M:%S') ===";
+	echo "=== [$1] $(date '+%H:%M:%S') ===" | tee -a "$LOG_FILE";
 }
 log_ok()
 {
-	echo "=== [$1] OK ===";
+	echo "=== [$1] OK ===" | tee -a "$LOG_FILE";
 }
