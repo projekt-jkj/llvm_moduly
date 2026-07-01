6db386a73e70aad7e3fcdf45de48b6c4765d61b2
Basvas j.k.j <basvas@seznam.cz>
2026-07-01 22:00:45 +0200

    Zrychlení klonování LLVM pomocí sparse checkout.

--- init_repositories.sh
+++ init_repositories.sh
@@ -5,19 +5,45 @@ set -ue;
 source "./argument_parser.sh";
 source "./helpers.sh";
 
+LLVM_SUBDIRECTORIES=(
+	clang
+	cmake
+	compiler-rt
+	libcxx
+	libcxxabi
+	libunwind
+	lld
+	llvm
+	runtimes
+	third-party
+);
+if [ "$BUILD_CLANG_TOOLS" = "ON" ]
+then
+	LLVM_SUBDIRECTORIES+=(clang-tools-extra);
+fi
+if [ "$BUILD_LLDB" = "ON" ]
+then
+	LLVM_SUBDIRECTORIES+=(lldb);
+fi
+
 clone()
 {
 	log_begin "Cloning $2";
+	[ ! -d "$3" ] && echo "ano $2 $3" || echo "ne $2 $3";
+
 	if [ ! -d "$3" ]
 	then
-		git clone --config core.autocrlf=false --config advice.detachedHead=false --depth 1 -q -b "$1" "$2" "$3" >>"$LOG_FILE";
+		git clone --config core.autocrlf=false --config advice.detachedHead=false --depth 1 -q -b "$@" >>"$LOG_FILE";
 	fi
 	log_ok "Cloning $2";
 }
 
-clone "$LLVM_TAG" "https://github.com/llvm/llvm-project.git" "$LLVM_SOURCE";
+clone "$LLVM_TAG" "https://github.com/llvm/llvm-project.git" "$LLVM_SOURCE" "--sparse";
 
 if [ -v MINGW_TAG ]
 then
 	clone "$MINGW_TAG" "https://github.com/mingw-w64/mingw-w64.git" "$MINGW_SOURCE";
 fi
+
+cd "$LLVM_SOURCE";
+git sparse-checkout add "${LLVM_SUBDIRECTORIES[@]}";
